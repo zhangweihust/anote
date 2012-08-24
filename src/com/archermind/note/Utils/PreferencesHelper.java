fe169f5745e4521917ac2194702a298bee9b94eb
wei.feng <000825@archermind.com>
2012-08-24 18:02:21 +0800

    app自动更新
    Change-Id: Ib2f760611d4a867fe2f83ecf89143c80c8f4320a
    Signed-off-by: wei.feng <000825@archermind.com>

--- src/com/archermind/note/Utils/PreferencesHelper.java
+++ src/com/archermind/note/Utils/PreferencesHelper.java
@@ -12,6 +12,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.AvoidXfermode.Mode;
+import android.graphics.Bitmap.Config;
 
 public class PreferencesHelper {
 	
@@ -68,10 +76,35 @@ public class PreferencesHelper {
 				mUserAvatarBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.my_photo);
 			}
 		}
+		mUserAvatarBitmap = toRoundCorner(mUserAvatarBitmap, 15);
 		mIsLoadAvatar = true;
 		
 		return mUserAvatarBitmap;
 	}
+	
+	public static Bitmap toRoundCorner(Bitmap bitmap, int pixels) {  
+		if (bitmap == null)
+			return null;
+        
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);  
+        Canvas canvas = new Canvas(output);  
+  
+        final int color = 0xff424242;  
+        final Paint paint = new Paint();  
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());  
+        final RectF rectF = new RectF(rect);  
+        final float roundPx = pixels;  
+  
+        paint.setAntiAlias(true);  
+        canvas.drawARGB(0, 0, 0, 0);  
+        paint.setColor(color);  
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);  
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));  
+        canvas.drawBitmap(bitmap, rect, rect, paint);  
+  
+        return output;  
+    }
 	
 	public static SharedPreferences getSharedPreferences(Context context, int mode) {
 		return context.getSharedPreferences(XML_NAME, mode);
