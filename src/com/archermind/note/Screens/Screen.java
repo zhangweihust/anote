ae3a48cdd71df3db50e6c874d68da2084237e7d0
anote_yt <001325@archermind.com>
2012-08-28 09:42:16 +0800

    使应用程序不支持横竖屏切换
    Change-Id: I477a77ac0fd532f113b281cbaf5d5a39ed39bbdb
    Signed-off-by: anote_yt <001325@archermind.com>

--- src/com/archermind/note/Screens/Screen.java
+++ src/com/archermind/note/Screens/Screen.java
@@ -2,9 +2,20 @@ package com.archermind.note.Screens;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
 
 public class Screen extends Activity {
 	private ProgressDialog mProgressDialog;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);  
+	}
 	/**
 	 * 显示等待框
 	 * 
